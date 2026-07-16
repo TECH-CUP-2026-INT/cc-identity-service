75c1555fb505da56459702a0752101ad59ddeaac
Caliche96 <rojasriverocarlosduban@gmail.com>
2026-07-16 14:31:45 -0500

    chore: eliminar archivos no utilizados o deprecados

--- src/main/java/co/edu/escuelaing/techcup/identity/infrastructure/adapter/out/persistence/repository/AuditEventMongoRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.escuelaing.techcup.identity.infrastructure.adapter.out.persistence.repository;
-
-import co.edu.escuelaing.techcup.identity.infrastructure.adapter.out.persistence.document.AuditEventDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AuditEventMongoRepository extends MongoRepository<AuditEventDocument, String> {
-}
