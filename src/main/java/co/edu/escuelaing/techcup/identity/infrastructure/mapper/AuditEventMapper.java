8ec60640285f1a9d1502c32da4133a4b81a7c6b2
Caliche96 <rojasriverocarlosduban@gmail.com>
2026-07-16 14:52:50 -0500

    fix: eliminar mappers que referencian documentos de persistencia eliminados

--- src/main/java/co/edu/escuelaing/techcup/identity/infrastructure/mapper/AuditEventMapper.java
+++ src/main/java/co/edu/escuelaing/techcup/identity/infrastructure/mapper/AuditEventMapper.java
@@ -2,15 +2,10 @@ package co.edu.escuelaing.techcup.identity.infrastructure.mapper;
 
 import co.edu.escuelaing.techcup.identity.domain.model.AuditEvent;
 import co.edu.escuelaing.techcup.identity.infrastructure.adapter.in.rest.dto.response.AuditEventResponse;
-import co.edu.escuelaing.techcup.identity.infrastructure.adapter.out.persistence.document.AuditEventDocument;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface AuditEventMapper {
-
-    AuditEventDocument toDocument(AuditEvent event);
-
-    AuditEvent toDomain(AuditEventDocument document);
 
     AuditEventResponse toResponse(AuditEvent event);
 }
